0e21b863259574546d7c4d815ae179b9bce1a1fb
Adam Jonas <adamjonas25@gmail.com>
2023-09-02 16:52:42 +0100

    Add fleet settings

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -2,33 +2,36 @@
 # Aliasses #
 #==========#
 
-alias pc="printColors"
-alias c="clear"
+alias pc='printColors'
+alias c='clear'
 
 # ls with colour
-alias ll="exa -al -h"
-alias lm="exa -alt modified"
-alias lc="exa -alt created"
-alias lch="exa -alt changed"
-alias lta="exa -alt accessed"
-alias la="exa -la"
+alias ll='exa -al -h'
+alias lm='exa -alt modified'
+alias lc='exa -alt created'
+alias lch='exa -alt changed'
+alias lta='exa -alt accessed'
+alias la='exa -la'
+
+alias trash='mv -f ~/.Trash'
+alias mv='mv -i'
 
 # cat & less with colour
-alias cat="ccat"
-alias less="cless"
+alias cat='ccat'
+alias less='cless'
 
 # Pretty print the path
-alias path='echo $PATH | tr -s ":" "\n"'
+# alias path='echo $PATH | tr -s ":" "\n"'
 alias path='echo -e ${PATH//:/\\n}'
 
 # Network
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en0"
+alias ip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias localip='ipconfig getifaddr en0'
 
 # Brew dependencies
-alias brewdeps="brew leaves | xargs brew deps --include-build --tree"
+alias brewdeps='brew leaves | xargs brew deps --include-build --tree'
 
-alias vz="vim ~/.config/zsh/.zshrc"
-alias fz="fleet ~/.config/zsh/zsh-config"
-alias cz="code ~/.config/zsh/zsh-config"
-
+# ZSH
+alias vz='vim ~/.config/zsh/.zshrc'
+alias fz='fleet ~/.config/zsh/zsh-config'
+alias cz='code ~/.config/zsh/zsh-config'
--- zsh/.functions.zsh
+++ zsh/.functions.zsh
@@ -5,3 +5,10 @@
 printColors() {
     for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+# Git
+gacp() {
+    git add .
+    git commit -m "$1"
+    git push
+}
