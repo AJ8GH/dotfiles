7c9c542204429bde8b0ffef8685f868f0890b55b
Adam Jonas <adamjonas25@gmail.com>
2025-01-17 22:17:55 +0000

    Improve dap config

--- nvim/lua/plugins/dap/nvim-dap.lua
+++ nvim/lua/plugins/dap/nvim-dap.lua
@@ -16,21 +16,31 @@ return {
     ui.setup()
     dap_go.setup()
 
-    vim.keymap.set("n", "<leader>dgt", dap_go.debug_test)
-    vim.keymap.set("n", "<leader>dgl", dap_go.debug_last_test)
+    vim.keymap.set("n", "¬", dap_go.debug_last_test)
+    vim.keymap.set("n", "†", dap_go.debug_test)
+    vim.keymap.set("n", "∂", dap_go.debug_test)
+    vim.keymap.set("n", "ø", dap.step_over)
+    vim.keymap.set("n", "‘", dap.step_over)
+    vim.keymap.set("n", "’", dap.continue)
+    vim.keymap.set("n", "≈", ui.toggle)
+    vim.keymap.set("n", "®", dap.restart)
+    vim.keymap.set("n", "Ù", dap.disconnect)
 
-    vim.keymap.set("n", "<leader>dso", dap.step_over)
-    vim.keymap.set("n", "<leader>dc", dap.continue)
-    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
-    vim.keymap.set("n", "<leader>dbr", dap.restart)
-    vim.keymap.set("n", "<leader>dbd", dap.disconnect)
+    vim.keymap.set("n", "<leader>dt", dap_go.debug_test)
+    vim.keymap.set("n", "<leader>dl", dap_go.debug_last_test)
+    vim.keymap.set("n", "<leader>do", dap.step_over)
+    vim.keymap.set("n", "<leader>dd", dap.continue)
+    -- use persistent breakpoint instead
+    -- vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+    vim.keymap.set("n", "<leader>dr", dap.restart)
+    vim.keymap.set("n", "<leader>dx", dap.disconnect)
     vim.keymap.set("n", "<leader>di", dap.step_into)
-    vim.keymap.set("n", "<leader>dou", dap.step_out)
-    vim.keymap.set("n", "<leader>dsb", dap.step_back)
-    vim.keymap.set("n", "<leader>dro", dap.repl.open)
-    vim.keymap.set("n", "<leader>drt", dap.repl.toggle)
-    vim.keymap.set("n", "<leader>drc", dap.repl.close)
-    vim.keymap.set("n", "<leader>dtc", dap.run_to_cursor)
+    vim.keymap.set("n", "<leader>du", dap.step_out)
+    -- vim.keymap.set("n", "<leader>dsb", dap.step_back)
+    -- vim.keymap.set("n", "<leader>dro", dap.repl.open)
+    -- vim.keymap.set("n", "<leader>drc", dap.repl.close)
+    vim.keymap.set("n", "<leader>de", dap.repl.toggle)
+    vim.keymap.set("n", "<leader>dc", dap.run_to_cursor)
 
     -- Eval var under cursor
     vim.keymap.set("n", "<space>de", function()
@@ -39,11 +49,20 @@ return {
     vim.keymap.set("n", "<leader>duc", ui.close)
     vim.keymap.set("n", "<leader>duo", ui.open)
     vim.keymap.set("n", "<leader>dut", ui.toggle)
+
+    local types_enabled = true
+    local toggle_types = function()
+      types_enabled = not types_enabled
+      ui.update_render({ max_type_length = types_enabled and -1 or 0 })
+    end
+    vim.keymap.set("n", "<leader>ut", toggle_types)
     dap.listeners.before.attach.dapui_config = function()
       ui.open()
+      toggle_types()
     end
     dap.listeners.before.launch.dapui_config = function()
       ui.open()
+      toggle_types()
     end
     dap.listeners.before.event_terminated.dapui_config = function()
       ui.close()
--- nvim/lua/plugins/dap/persistent-checkpoints.lua
+++ nvim/lua/plugins/dap/persistent-checkpoints.lua
@@ -1,10 +1,11 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 -- Save breakpoints to file automatically.
+keymap("n", "π", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>", opts)
 keymap("n", "<leader>dp", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>", opts)
-keymap("n", "<>", "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<cr>", opts)
-keymap("n", "<YourKey3>", "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>", opts)
-keymap("n", "<YourKey4>", "<cmd>lua require('persistent-breakpoints.api').set_log_point()<cr>", opts)
+keymap("n", "<leader>dcp", "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<cr>", opts)
+keymap("n", "<leader>cap", "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>", opts)
+-- keymap("n", "<YourKey4>", "<cmd>lua require('persistent-breakpoints.api').set_log_point()<cr>", opts)
 
 return {
   "Weissle/persistent-breakpoints.nvim",
