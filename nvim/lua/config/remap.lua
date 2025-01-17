40a6e6e9c00c14905ec0ee5ffea61212d74024a0
Adam Jonas <adamjonas25@gmail.com>
2025-01-17 23:09:08 +0000

    Update mappings

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,9 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>q", "<cmd>qa<CR>")
 
--- Move visual blocks
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move visual block down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move visual block up" })
 
 -- Improved navigation
 -- vim.keymap.set("n", "J", "mzJ`z")
@@ -12,25 +11,19 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- vim.keymap.set("n", "n", "nzzzv")
 -- vim.keymap.set("n", "N", "Nzzzv")
 
--- Netrw
-vim.keymap.set("n", "<leader>lex", "<cmd>Lexplore<CR>")
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>lex", "<cmd>Lexplore<CR>", { desc = "Toggle Netrw in vertical split" })
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex, { desc = "Open Netrw" })
 
--- Put preserving buffer
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Put preserving buffer" })
 
--- Yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to system clipboard" })
 
--- Put from system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
-vim.keymap.set("n", "<leader>P", [["+P]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Put from system clipboard" })
+vim.keymap.set("n", "<leader>P", [["+P]], { desc = "Put from system clipboard" })
 
 vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
-
--- Preserve visual block changes on Ctrl-c
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Preserve visual block changes on Ctrl-c" })
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -41,16 +34,25 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
--- Create command template to replace current word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Create command template to replace current word" }
+)
 
--- Make current file executable
-vim.keymap.set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file executable" })
 
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 vim.keymap.set("n", "<leader>ea", 'oassert.NoError(err, "")<Esc>F";a')
 vim.keymap.set("n", "<leader>ef", 'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj')
 vim.keymap.set("n", "<leader>el", 'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i')
 
--- change working directory to the location of the current file
-vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>:pwd<CR>")
+vim.keymap.set(
+  "n",
+  "<leader>cd",
+  ":cd %:p:h<CR>:pwd<CR>",
+  { desc = "Change working directory to the location of the current file" }
+)
+
+vim.keymap.set("n", "å", "gT", { desc = "Change tab" })
