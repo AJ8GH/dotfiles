d8e30eb5944823be64d9ae56ef32a3f4780a6b46
Adam Jonas <adamjonas25@gmail.com>
2025-01-18 00:03:39 +0000

    Update zenmode config

--- nvim/lua/plugins/ui/zenmode.lua
+++ nvim/lua/plugins/ui/zenmode.lua
@@ -1,9 +1,5 @@
 local toggle = function()
-  require("zen-mode").toggle({
-    window = {
-      width = 0.98, -- width will be 85% of the editor width
-    },
-  })
+  require("zen-mode").toggle({})
 end
 vim.keymap.set("n", "Ω", toggle, { desc = "Toggle zen mode" })
 
@@ -11,12 +7,12 @@ return {
   "folke/zen-mode.nvim",
   opts = {
     window = {
-      backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+      backdrop = 0.45, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
       -- height and width can be:
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = 120, -- width of the Zen window
+      width = 0.94, -- width of the Zen window
       height = 1, -- height of the Zen window
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
@@ -85,8 +81,12 @@ return {
       },
     },
     -- callback where you can add custom code when the Zen window opens
-    on_open = function(win) end,
+    on_open = function(win)
+      require("barbecue.ui").toggle()
+    end,
     -- callback where you can add custom code when the Zen window closes
-    on_close = function() end,
+    on_close = function()
+      require("barbecue.ui").toggle()
+    end,
   },
 }
